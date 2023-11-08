ed76a40a0ad81cb8d722341efd4f6565f3ae2e47
hermeswaldemarin <hermes@absmartly.com>
2023-11-08 18:13:00 +0000

    Adding Github pipelines

--- core-api/src/main/java/com/absmartly/sdk/json/CustomFieldValue.java
+++ core-api/src/main/java/com/absmartly/sdk/json/CustomFieldValue.java
@@ -1,15 +1,10 @@
 package com.absmartly.sdk.json;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Getter
-@Setter
 public class CustomFieldValue {
 	private String name;
 	private String type;
@@ -48,5 +43,29 @@ public class CustomFieldValue {
 				", type='" + type + '\'' +
 				", value='" + value + '\'' +
 				'}';
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
 	}
 }
